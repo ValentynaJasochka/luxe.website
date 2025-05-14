8ebbf456b6d1956ab44beda833dae8001350fb18
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-05-14 22:24:32 +0300

    update title

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const title = document.querySelector('.js-title');
 const userName=document.querySelector('.js-username');
-console.log(userName.value);
-console.log(title.textContent)
+console.log(!!userName.value);
+
 title.textContent = `Hello ${userName.value}`;
+console.log(title.textContent)
+if (!!userName.value) {
+  console.log(title.textContent.toUpperCase());
+  title.textContent=title.textContent.toUpperCase()
+}
+else{
+  title.textContent = `Write your Name`;
+}
