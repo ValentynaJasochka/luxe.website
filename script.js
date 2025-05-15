5caa12f525a06b37ee6ca457ad54dda2caae8166
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-05-15 18:59:33 +0300

    data attributes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const title = document.querySelector('.js-title');
 const userName=document.querySelector('.js-username');
+const comment =document.querySelector('.js-comment')
 console.log(!!userName.value);
 
 title.textContent = `Hello ${userName.value}`;
@@ -8,9 +9,14 @@ if (!!userName.value) {
   console.log(title.textContent.toUpperCase());
   title.textContent=title.textContent.toUpperCase()
   title.classList.add("title-color")
+  comment.hidden = true;
+  console.log(comment);
+
 }
 else{
   title.textContent = `Write your Name`;
-  title.classList.remove("title-color")
+  title.classList.remove("title-color");
+  comment.hidden = false;
+  console.log(comment);
 }
 
