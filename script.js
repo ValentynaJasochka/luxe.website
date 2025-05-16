af1bcb405d146e56ec329700f9bfdaa31acc45c5
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2025-05-16 18:45:40 +0300

    create element

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-const title = document.querySelector('.js-title');
-const userName=document.querySelector('.js-username');
-const comment =document.querySelector('.js-comment')
-const list = document.querySelector('.js-list')
-const goods = [...list.children]
-console.log(goods);
+const title = document.querySelector(".js-title");
+const userName = document.querySelector(".js-username");
+const comment = document.querySelector(".js-comment");
+const list = document.querySelector(".js-list");
 
 title.textContent = `Hello ${userName.value}`;
-console.log(title.textContent)
-if (!!userName.value) {
-  console.log(title.textContent.toUpperCase());
-  title.textContent=title.textContent.toUpperCase()
-  title.classList.add("title-color")
-  comment.hidden = true;
-  console.log(comment);
 
-}
-else{
+if (!!userName.value) {
+  title.textContent = title.textContent.toUpperCase();
+  title.classList.add("title-color");
+  comment.hidden = true;
+} else {
   title.textContent = `Write your Name`;
   title.classList.remove("title-color");
   comment.hidden = false;
-  console.log(comment);
 }
- goods.forEach(good => {
-  good.textContent = `tomato`
- });
+
+const li = document.createElement("li");
+
+list.appendChild(li);
+
+const goods = [...list.children];
+
+goods.forEach((good) => {
+  good.textContent = `tomato`;
+  good.classList.add('item')
+});
